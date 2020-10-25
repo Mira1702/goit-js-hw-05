d6aa3678d6017def72c48e3eb56ad85da4294244
Myroslava Nikolaienko <kvi2nya@gmail.com>
2020-10-25 20:50:40 +0200

    Решена задача №05

--- task-05-05.js
+++ task-05-05.js
@@ -1,14 +1,50 @@
 class Car {
     // Write code under this line
-     static getSpecs(){}
-     constructor(){}
-     get price() {}
-     set price(value) {}
-     turnOn() {}
-     turnOff() {}
-     accelerate(value) {}
-     decelerate(value) {}
-     drive(hours) {}
+     static getSpecs(car){
+      return `maxSpeed: ${car.maxSpeed}, speed: ${car.speed}, isOn: ${car.isOn}, distance: ${car.distance}, price: ${car.price}`; 
+        
+     }
+     
+     constructor(objCar){
+        this.maxSpeed = objCar.maxSpeed;
+        this.speed = 0;
+        this.isOn = false;
+        this.distance = 0;
+        this.price = objCar.price;
+     }
+     get price() {
+        return this._price;
+     }
+     set price(value) {
+      this._price = value;
+     }
+     turnOn() {
+        this.isOn = true;
+     }
+     turnOff() {
+      this.isOn = false;
+     }
+     accelerate(value) {
+      if (this.speed + value <= this.maxSpeed) {
+         this.speed += value;
+        } else {
+          this.speed = this.maxSpeed;
+         }
+         return this.speed;
+     }
+     decelerate(value) {
+        if (this.speed - value >=0){
+           return this.speed -= value;
+        } else {
+         this.speed = 0;
+        }
+        
+     }
+     drive(hours) {
+        if (this.isOn){
+           this.distance += hours*this.speed;
+        }
+     }
 }
     
 const mustang = new Car({ maxSpeed: 200, price: 2000 });
